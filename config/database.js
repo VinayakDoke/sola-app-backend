7590b901ab87bb5b3549f651876da090c4ffb11a
vinayakdoke1 <vinayak.doke@bynaric.in>
2025-10-20 18:56:07 +0530

    updatec

--- config/database.js
+++ config/database.js
@@ -4,14 +4,14 @@ import { Sequelize } from "sequelize";
 dotenv.config();
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT || "mysql",
-    logging: false
-  }
+  // process.env.DB_NAME,
+  // process.env.DB_USER,
+  // process.env.DB_PASSWORD,
+  // {
+  //   host: process.env.DB_HOST,
+  //   dialect: process.env.DB_DIALECT || "mysql",
+  //   logging: false
+  // }
 );
 
 export default sequelize;
