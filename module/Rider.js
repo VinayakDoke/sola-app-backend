4535c6c06dae5b598546700519589a2e17f3d27b
vinayakdoke1 <vinayak.doke@bynaric.in>
2025-10-24 10:46:45 +0530

    updated

--- module/Rider.js
+++ module/Rider.js
@@ -1,6 +1,6 @@
 // models/Rider.js
-import { Model, DataTypes } from 'sequelize';
-import sequelize from '../config/database.js'; // adjust the path to your Sequelize config
+import { Model, DataTypes } from 'sequelize'
+import sequelize from '../config/database.js' // adjust the path to your Sequelize config
 
 class Rider extends Model {
   // You can add instance or class-level methods here if needed
@@ -11,89 +11,97 @@ Rider.init(
     id: {
       type: DataTypes.INTEGER,
       primaryKey: true,
-      autoIncrement: true,
+      autoIncrement: true
     },
     vender_id: {
       type: DataTypes.INTEGER,
-      allowNull: false,
+      allowNull: false
     },
     first_name: {
       type: DataTypes.STRING,
-      allowNull: true,
+      allowNull: true
     },
     middle_name: {
       type: DataTypes.STRING,
-      allowNull: true,
+      allowNull: true
     },
     last_name: {
       type: DataTypes.STRING,
-      allowNull: true,
+      allowNull: true
     },
     date_of_birth: {
       type: DataTypes.DATE,
-      allowNull: true,
+      allowNull: true
     },
     mobile: {
       type: DataTypes.STRING(15),
-      allowNull: true,
+      allowNull: true
     },
     alternate_mobile: {
       type: DataTypes.STRING(15),
-      allowNull: true,
+      allowNull: true
     },
     adhar_number: {
       type: DataTypes.STRING(20),
-      allowNull: true,
+      allowNull: true
     },
     driving_license: {
       type: DataTypes.STRING(30),
-      allowNull: true,
+      allowNull: true
     },
     driving_license_expiry: {
       type: DataTypes.DATE,
-      allowNull: true,
+      allowNull: true
     },
     driving_experience: {
       type: DataTypes.INTEGER,
-      allowNull: true,
+      allowNull: true
     },
     address: {
       type: DataTypes.TEXT,
-      allowNull: true,
+      allowNull: true
     },
     pic: {
       type: DataTypes.STRING,
-      allowNull: true,
+      allowNull: true
     },
     adhar_photo: {
       type: DataTypes.STRING,
-      allowNull: true,
+      allowNull: true
     },
     driving_licence_photo: {
       type: DataTypes.STRING,
-      allowNull: true,
+      allowNull: true
     },
     createdAt: {
       type: DataTypes.DATE,
       allowNull: false,
-      defaultValue: DataTypes.NOW,
+      defaultValue: DataTypes.NOW
     },
     updatedAt: {
       type: DataTypes.DATE,
-      allowNull: true,
+      allowNull: true
     },
     deletedAt: {
       type: DataTypes.DATE,
-      allowNull: true,
+      allowNull: true
     },
+    registration_status: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    approved: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    }
   },
   {
     sequelize,
     modelName: 'Rider',
     tableName: 'rider',
     paranoid: true, // enable soft deletes
-    timestamps: true, // auto-manage createdAt & updatedAt
+    timestamps: true // auto-manage createdAt & updatedAt
   }
-);
+)
 
-export default Rider;
+export default Rider
